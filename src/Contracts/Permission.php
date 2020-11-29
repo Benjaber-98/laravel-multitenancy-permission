ec804adc56876c94cc89ca5f6a88293167eee9df
Mahmoud Ben Jabir <32901098+Benjaber-98@users.noreply.github.com>
2020-11-29 22:26:26 +0200

    Update Permission.php

--- src/Contracts/Permission.php
+++ src/Contracts/Permission.php
@@ -6,12 +6,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 interface Permission
 {
-    /**
-     * A permission can be applied to roles.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function roles(): BelongsToMany;
 
     /**
      * Find a permission by its name.
